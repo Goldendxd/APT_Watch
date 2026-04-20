c9ba8e1805f822be6e0589454c6fff80ebee6f3a
ashishthapa1414-bit <ashishthapa1414@gmail.com>
2026-04-20 12:22:08 +0545

    Add GiftingServlet and JSP for gifting feature; remove unused CartDao and CartDaoImpl

---
+++ src/main/java/com/alughadi/dao/CartDao.java
@@ -0,0 +1,15 @@
+//package com.alughadi.dao;
+//
+//import com.alughadi.entity.Cart;
+//import java.util.List;
+//
+//public interface CartDao {
+//
+//    void       addToCart(int userId, int productId, int quantity);
+//    List<Cart> getCartItems(int userId);
+//    int        getCartCount(int userId);
+//    double     getGrandTotal(int userId);
+//    void       removeFromCart(int cartId, int userId);
+//    void       updateQuantity(int cartId, int userId, int quantity);
+//    void       clearCart(int userId);
+//}
